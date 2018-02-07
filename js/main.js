eacb6756596bae12126cb4a2322a3ff2c9180874
zubcarz <zubcarz@gmail.com>
2018-02-07 00:23:27 -0500

    Draw proposed  figure

--- js/main.js
+++ js/main.js
@@ -85,29 +85,57 @@ function initBuffers(gl) {
     // Now create an array of positions for the cube.
 
     const positions  = [
-        // Cara delantera
-        -0.5, -0.5,  0.5,
-        0.5, -0.5,  0.5,
-        0.5,  0.5,  0.5,
-        -0.5,  0.5,  0.5,
 
-        // Cara trasera
-        -0.5, -0.5, -0.5,
-        -0.5,  0.5, -0.5,
-        0.5,  0.5, -0.5,
-        0.5, -0.5, -0.5,
+        ///// Top Pyramid //////
+        // Front face
+        -0.5, 0.5,  0.5,
+        0.5, 0.5,  0.5,
+        1.0,  1.5,  1.0,
+        -1.0,  1.5,  1.0,
 
-        // Top face
-        -0.5,  0.5, -0.5,
-        -0.5,  0.5,  0.5,
-        0.5,  0.5,  0.5,
-        0.5,  0.5, -0.5,
+        // Back face
+        -0.5, 0.5, -0.5,
+        -1.0,  1.5, -1.0,
+        1.0,  1.5, -1.0,
+        0.5, 0.5, -0.5,
+
+        //Top pyramid
+        -1.0,  1.5, -1.0,
+        -1.0,  1.5,  1.0,
+        1.0,  1.5,  1.0,
+        1.0,  1.5, -1.0,
 
         // Bottom face
+        -1.0, -1.5, -1.0,
+        1.0, -1.5, -1.0,
+        1.0, -1.5,  1.0,
+        -1.0, -1.5,  1.0,
+
+        // Right face
+        0.5, 0.5, -0.5,
+        1.0,  1.5, -1.0,
+        1.0,  1.5,  1.0,
+        0.5, 0.5,  0.5,
+
+        // Left face
+        -0.5, 0.5, -0.5,
+        -0.5, 0.5,  0.5,
+        -1.0,  1.5,  1.0,
+        -1.0,  1.5, -1.0,
+
+        ////// Cube ////////
+
+        // Front face
+        -0.5, -0.5,  0.5, //24
+        0.5, -0.5,  0.5, //25
+        0.5,  0.5,  0.5, //26
+        -0.5,  0.5,  0.5, //27
+
+        // Back face
         -0.5, -0.5, -0.5,
+        -0.5,  0.5, -0.5,
+        0.5,  0.5, -0.5,
         0.5, -0.5, -0.5,
-        0.5, -0.5,  0.5,
-        -0.5, -0.5,  0.5,
 
         // Right face
         0.5, -0.5, -0.5,
@@ -119,18 +147,56 @@ function initBuffers(gl) {
         -0.5, -0.5, -0.5,
         -0.5, -0.5,  0.5,
         -0.5,  0.5,  0.5,
-        -0.5,  0.5, -0.5
+        -0.5,  0.5, -0.5,
+
+        ///// Down Pyramid //////
+        // Front
+        -1.0, -1.5,  1.0,
+        1.0, -1.5,  1.0,
+        0.5,  -0.5,  0.5,
+        -0.5, -0.5,  0.5,
+
+        // Back
+        -1.0, -1.5, -1.0,
+        -0.5,  -0.5, -0.5,
+        0.5,  -0.5, -0.5,
+        1.0, -1.5, -1.0,
+
+        // Right face
+        1.0, -1.5, -1.0,
+        0.5,  -0.5, -0.5,
+        0.5,  -0.5,  0.5,
+        1.0, -1.5,  1.0,
+
+        // Left face
+        -1.0, -1.5, -1.0,
+        -1.0, -1.5,  1.0,
+        -0.5,  -0.5,  0.5,
+        -0.5,  -0.5, -0.5
+
     ];
+
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
     const faceColors = [
-        [1.0,  1.0,  1.0,  1.0],    // Front face: white
-        [1.0,  0.0,  0.0,  1.0],    // Back face: red
-        [0.0,  1.0,  0.0,  1.0],    // Top face: green
-        [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-        [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-        [1.0,  0.0,  1.0,  1.0],    // Left face: purple
+        // Pyramid Color
+        [123/255,  156/255,  187/255,  1.0],    // Front face: white
+        [61/255,  87/255,  111/255,  1.0],    // Back face: red
+        [0.0,  0.0,  0.0,  1.0],    // Top face: green
+        [0.0,  0.0,  0.0,  1.0],    // Bottom face: blue
+        [187/255,  142/255,  66/255,  1.0],    // Right face: yellow
+        [111/255,  92/255,  61/255,  1.0],    // Left face: purple
+        // Cube Color
+        [111/255,  92/255,  61/255,  1.0],   // Front face
+        [187/255,  142/255,  66/255,  1.0],  // Back face
+        [123/255,  156/255,  187/255,  1.0],   // Right face
+        [61/255,  87/255,  111/255,  1.0],  // Left  face
+        // Pyramid Color
+        [61/255,  87/255,  111/255,  1.0],  // Front face
+        [123/255,  156/255,  187/255,  1.0],   // Back face
+        [111/255,  92/255,  61/255,  1.0],   // Right face
+        [123/255,  156/255,  187/255,  1.0],  // Left  face
     ];
 
     // Convert the array of colors into a table for all the vertices.
@@ -157,12 +223,25 @@ function initBuffers(gl) {
 // para especificar cada posición en los tríangulos.
 
     var indices  = [
-        0,  1,  2,      0,  2,  3,    // enfrente
-        4,  5,  6,      4,  6,  7,    // atrás
-        8,  9,  10,     8,  10, 11,   // arriba
-        12, 13, 14,     12, 14, 15,   // fondo
-        16, 17, 18,     16, 18, 19,   // derecha
-        20, 21, 22,     20, 22, 23    // izquierda
+        // Pyramid UP
+        0,  1,  2,      0,  2,  3,    // Front
+        4,  5,  6,      4,  6,  7,    // Back
+        16, 17, 18,     16, 18, 19,   // Right
+        20, 21, 22,     20, 22, 23,    // left
+        // Cube
+        24, 25, 26,     24, 26, 27,     //  Front
+        28, 29, 30,     28, 30, 31,     //  Back
+        32, 33, 34,     32, 34, 35,     // Right
+        36, 37, 38,     36, 38, 39,      // Left
+        // Pyramid Down
+        40, 41, 42,     40, 42, 43,     //  Front
+        44, 45, 46,     44, 46, 47,     //  Back
+        48, 49, 50,     48, 50, 51,     // Right
+        52, 53, 54,     52, 54, 55,     // Left
+        // Top and Button
+        8,  9,  10,     8,  10, 11,   // Up
+        12, 13, 14,     12, 14, 15,   // Down
+
     ];
 
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,
@@ -274,7 +353,7 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
         false,
         modelViewMatrix);
     {
-        const vertexCount = 36;
+        const vertexCount = 84;
         const type = gl.UNSIGNED_SHORT;
         const offset = 0;
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
