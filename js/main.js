a2f4b2dec23c3112cb8d60741688ead4299cbe5f
zubcarz <zubcarz@gmail.com>
2018-02-06 19:26:21 -0500

    Rotate Element

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 "use strict";
+
 var gl;
+
+var squareRotation = 0.0;
+
 const vsSource = `
     attribute vec4 aVertexPosition;
     attribute vec4 aVertexColor;
@@ -110,7 +114,7 @@ function initBuffers(gl) {
     };
 }
 
-function drawScene(gl, programInfo, buffers) {
+function drawScene(gl, programInfo, buffers, deltaTime) {
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
     gl.clearDepth(1.0);                 // Clear everything
@@ -141,6 +145,12 @@ function drawScene(gl, programInfo, buffers) {
     mat4.translate(modelViewMatrix,     // destination matrix
         modelViewMatrix,     // matrix to translate
         [-0.0, 0.0, -6.0]);  // amount to translate
+
+    mat4.rotate(modelViewMatrix,  // destination matrix
+        modelViewMatrix,  // matrix to rotate
+        squareRotation,   // amount to rotate in radians
+        [0, 0, -1]);       // axis to rotate around
+
 
     // Tell WebGL how to pull out the positions from the position
     // buffer into the vertexPosition attribute.
@@ -199,6 +209,9 @@ function drawScene(gl, programInfo, buffers) {
         const vertexCount = 4;
         gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
     }
+
+    squareRotation += deltaTime;
+
 }
 
 function start() {
@@ -229,7 +242,17 @@ function start() {
     const buffers = initBuffers(gl);
 
     // Draw the scene
-    drawScene(gl, programInfo, buffers);
+    var then = 0;
+
+    // Draw the scene repeatedly
+    function render(now) {
+        now *= 0.001;  // convert to seconds
+        const deltaTime = now - then;
+        then = now;
+        drawScene(gl, programInfo, buffers, deltaTime);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
 
 //Program
